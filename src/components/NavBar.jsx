e4a701d0a62613013498a3a88b6b4164ba2c739a
DevepMau <oshiettecs@gmail.com>
2025-05-31 13:19:44 -0300

    Se creo la carpeta assets / Se agrego la imagen del logo / Se agrego un logo al NavBar / Se modifico el css del NavBar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,16 +1,24 @@
 import React from 'react'
 import './NavBar.css';
+import logo from '../assets/logo.png';
 
 export default function NavBar() {
   return (
     <nav className="navbar">
-      <ul>
-        <li><a href="#home">Inicio</a></li>
-        <li><a href="#services">Servicios</a></li>
-        <li><a href="#gallery">Galería</a></li>
-        <li><a href="#about">Nosotros</a></li>
-        <li><a href="#contact">Contacto</a></li>
-      </ul>
+        <div className='navbar-container'>
+            <div className='navbar-logo'>
+                <img src={logo} alt="Logo" />
+            </div>
+            <div className='navbar-links'>
+                <ul>
+                    <li><a href="#home">Inicio</a></li>
+                    <li><a href="#services">Servicios</a></li>
+                    <li><a href="#gallery">Galería</a></li>
+                    <li><a href="#about">Nosotros</a></li>
+                    <li><a href="#contact">Contacto</a></li>
+                </ul>
+            </div>
+        </div>
     </nav>
   );
 };
