22355488d68afa5eda45271726b9f85991431afb
DevepMau <oshiettecs@gmail.com>
2025-05-31 18:56:35 -0300

    Se creo ServicesData / Se agregaron imagenes a assets

---
+++ src/utils/ServicesData.js
@@ -0,0 +1,26 @@
+import cleanGlass from '../assets/imageServices/clean-glass.jpg';
+import cutGrass from '../assets/imageServices/cutting-grass.jpg';
+import electroRepair from '../assets/imageServices/electrician-repair.jpg';
+
+const services = [
+  {
+    id: 1,
+    title: 'Limpieza de vidrios',
+    image: cleanGlass,
+    description: 'Creamos sitios web modernos, rápidos y adaptables a cualquier dispositivo.'
+  },
+  {
+    id: 2,
+    title: 'Cortamos el cesped',
+    image: cutGrass,
+    description: 'Mejoramos la visibilidad de tu sitio web en los motores de búsqueda.'
+  },
+  {
+    id: 3,
+    title: 'Reparacion en general',
+    image: electroRepair,
+    description: 'Campañas efectivas en redes sociales, Google Ads y más.'
+  }
+];
+
+export default services;
