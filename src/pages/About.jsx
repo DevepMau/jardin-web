e791317d83fe80eeb16fbb4523b20881ec5f2591
DevepMau <oshiettecs@gmail.com>
2025-06-08 17:27:34 -0300

    Se cambiaron las imagenes de mision y vision / Se modifico la seccion central de about para poner en la misma linea la mision, el team y la vision.

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import './About.css';
 import groupPhoto from '../assets/imageAbout/team.jpg'; 
-import missionPhoo from '../assets/imageAbout/mission.jpg';
+import mision from '../assets/imageAbout/mision.png';
+import vision from '../assets/imageAbout/vision.png';
 import confianzaIcon from '../assets/fruit.png';
 import compromisoIcon from '../assets/fruit.png';
 import calidadIcon from '../assets/fruit.png';
@@ -14,40 +15,46 @@ export default function About() {
       <section className="about-hero">
         <h1>PROGRESANDO JUNTOS</h1>
       </section>
+      <section className='about-body'>
 
-      {/* Segunda sección: descripción + foto grupo */}
-      <section className="about-description">
-        <div className="text">
-          <h2>¿Quiénes somos?</h2>
-          <p>
-            El Santy es un emprendimiento familiar dedicado al corte de césped, podas menores y mantenimiento de jardines. 
-            Nos enfocamos en brindar un servicio responsable, prolijo y con cercanía, cuidando cada espacio verde con respeto 
-            por la naturaleza y el trabajo bien hecho.
-          </p>
-        </div>
-        <div className="image">
-          <img src={groupPhoto} alt="Equipo de trabajo" />
-        </div>
-      </section>
+        {/* Tercera sección: seccion mision / seccion vision */}
+        <section className="about-mision">
+          <div className="text">
+            <h2>Mision</h2>
+            <img src={mision} alt="Icono de mision" />
+            <p>
+              En El Santy, cuidamos tu patio como si fuera el nuestro. Nos dedicamos con ganas a cortar el pasto, 
+              podar y dejar los espacios verdes bien prolijos, para que puedas disfrutar sin preocuparte.
+            </p>
+          </div>
+        </section>
 
-      {/* Tercera sección: descripción + foto mision */}
-      <section className="about-mision">
-        <div className="image">
-          <img src={missionPhoo} alt="Equipo de trabajo" />
-        </div>
-        <div className="text">
-          <hr/>
-          <h2>Mision/Vision</h2>
-          <p>
-            En El Santy, cuidamos tu patio como si fuera el nuestro. Nos dedicamos con ganas a cortar el pasto, 
-            podar y dejar los espacios verdes bien prolijos, para que vos puedas disfrutar sin preocuparte.
-          </p>
-          <p>
-            Queremos ser la opción número uno del barrio cuando alguien piense en cortar el pasto o mantener su jardín. 
-            Crecer de a poco, con trabajo honesto, buena onda y haciendo bien las cosas.
-          </p>
-          <hr/>
-        </div>
+        {/* Segunda sección: descripción + foto grupo */}
+        <section className="about-description">
+          <div className="text">
+            <h2>¿Quiénes somos?</h2>
+            <p>
+              El Santy es un emprendimiento familiar dedicado al corte de césped, podas menores y mantenimiento de jardines. 
+              Nos enfocamos en brindar un servicio responsable, prolijo y con cercanía, cuidando cada espacio verde con respeto 
+              por la naturaleza y el trabajo bien hecho.
+            </p>
+          </div>
+          <div className="image">
+            <img src={groupPhoto} alt="Equipo de trabajo" />
+          </div>
+        </section>
+
+        {/* Tercera sección: seccion mision / seccion vision */}
+        <section className="about-vision">
+          <div className="text">
+            <h2>Vision</h2>
+            <img src={vision} alt="Icono de vision" />
+            <p>
+              Queremos ser la opción número uno del barrio cuando alguien piense en cortar el pasto o mantener su jardín. 
+              Crecer de a poco, con trabajo honesto, buena onda y haciendo bien las cosas.
+            </p>
+          </div>
+        </section>
       </section>
 
       {/* Cuarta sección: valores */}
