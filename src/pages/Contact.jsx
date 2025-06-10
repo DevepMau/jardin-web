4fcecc890ae786115c615e692d6f72c45f851c68
DevepMau <oshiettecs@gmail.com>
2025-06-10 18:49:45 -0300

    Se agrego el QR a contacto / Se agrego u boton para copiar el numero de telefono / ajustes visuales menores.

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -2,21 +2,30 @@ import React from 'react'
 import './Contact.css';
 import logo from '../assets/logo.png';
 import Nombre from '../assets/nombre.png';
+import QRCode from '../assets/imageContact/qr.jpg';
 import whatsappIcon from '../assets/imageContact/whatsapp.png';
 import mailIcon from '../assets/imageContact/mail.png';
 import instagramIcon from '../assets/imageContact/instagram.png';
+import copyIcon from '../assets/imageContact/copy.png';
 
 export default function Contact() {
   return (
     <div class="contact-container">
       <div className='contact-data'>
         <h1>¡CONTACTANOS!</h1>
+        <hr />
         <ul>
           <li><img src={logo} alt="Logo" className='data-logo'/></li>
           <li><img src={Nombre} alt='nombre' className='data-nombre'/></li>
           <li className='content'>
-            <img src={whatsappIcon} alt='icon-whatsapp' />
+            <img src={whatsappIcon} alt='icon-whatsapp'/>
             <p>11 6431 7622</p>
+            <button onClick={() => navigator.clipboard.writeText("1164317622")}>
+              <img src={copyIcon} alt='icon-copy'/>
+            </button>
+          </li>
+          <li className='qr-code'>
+            <img src={QRCode} alt='qr' />
           </li>
           <li className='content'>
             <img src={instagramIcon} alt='icon-instagram' />
