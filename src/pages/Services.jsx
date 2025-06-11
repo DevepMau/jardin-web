4637e58497afb09aee77b2478d21a5737ffb4064
DevepMau <oshiettecs@gmail.com>
2025-06-11 20:36:34 -0300

    Ajustes menores a Services

--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -3,8 +3,10 @@ import ServicesList from '../components/ServicesList'
 
 export default function Services() {
   return (
-    <div>
+    <div className='services-page'>
+        <div className='top-separator' />
         <ServicesList />
+        <div className='bottom-separator' />
     </div>
   )
 }
