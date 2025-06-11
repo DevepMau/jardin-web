8b3b843e0609277f6b3caa02c691f78718ec48ef
DevepMau <oshiettecs@gmail.com>
2025-06-11 20:28:54 -0300

    Ajustes en el css de Contact / Cambios al formulario / separador con degrade de secciones

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -11,9 +11,24 @@ import copyIcon from '../assets/imageContact/copy.png';
 export default function Contact() {
   return (
     <div class="contact-container">
-      <div className='contact-data'>
-        <h1>CONTACTANOS</h1>
+      <div className='top-separator' />
+      <div className='section-1'>
+        <h2>CONTACTAME</h2>
         <hr />
+        <div className='invite'>
+          <p className='invite-1'>
+          Estamos para ayudarte. Podés comunicarte con nosotros a través de 
+          WhatsApp, correo electrónico o 
+          Instagram, y con gusto responderemos todas tus consultas.
+          </p>
+          <p className='invite-2'>
+          También podés completar el siguiente formulario para solicitar un 
+          <strong> presupuesto sin compromiso</strong>. ¡Te responderemos lo antes posible!
+          </p>
+        </div>
+      </div>
+      <div className='section-2'>
+        <div className='contact-data-section'>
         <ul>
           <li><img src={logo} alt="Logo" className='data-logo'/></li>
           <li><img src={Nombre} alt='nombre' className='data-nombre'/></li>
@@ -36,18 +51,26 @@ export default function Contact() {
             <p>servicioselsanty@gmail.com</p>
           </li>
         </ul>
+        </div>
+        <div class="contact-form-section">
+          <h1>¡PEDI TU PRESUPUESTO!</h1>
+          <form action="https://formsubmit.co/servicioselsanty@gmail.com" method="POST" id="contact-form">
+            <h3>Nombre completo</h3>
+            <input type="text" name="nombre" required />
+            <h3>Email</h3>
+            <input type="email" name="email"/>
+            <h3>Telefono</h3>
+            <input type="number" name="telefono" required />
+            <h3>Asunto</h3>
+            <input type="text" name="nombre" placeholder='¿Que servicio buscas?' required />
+            <h3>Mensaje</h3>
+            <textarea name="mensaje" placeholder="Contanos qué necesitás" required></textarea>
+            <input type="hidden" name="_next" value="http://localhost:3000"></input>
+            <button type="submit">Enviar</button>
+          </form>
+        </div>
       </div>
-    <div class="contact-form-section">
-      <h1>¡PEDI TU PRESUPUESTO!</h1>
-      <form action="https://formsubmit.co/servicioselsanty@gmail.com" method="POST" id="contact-form">
-        <input type="text" name="nombre" placeholder="Tu nombre" required />
-        <input type="email" name="email" placeholder="Tu email" required />
-        <input type="text" name="localidad" placeholder="Localidad donde vives" />
-        <input type="text" name="direccion" placeholder="Tu direccion" />
-        <textarea name="mensaje" placeholder="Contanos qué necesitás" required></textarea>
-        <button type="submit">Enviar</button>
-      </form>
+      <div className='bottom-separator' />
     </div>
-  </div>
   )
 }
